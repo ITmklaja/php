a1cc2fa15fa0af618b161dafa9ca5dacadcea3e2
Maciek Klaja <93444048+ITmklaja@users.noreply.github.com>
2022-11-01 21:12:49 +0100

    Update index.php

--- index.php
+++ index.php
@@ -218,6 +218,7 @@
 
         echo "dzisiejsza data: ", date("d/m/Y"), ", ", date("l");
 
+        date_default_timezone_set("Europe/Lisbon");
         $data = getdate();
         $seconds = $data["seconds"];
         $minutes = $data["minutes"];
@@ -230,6 +231,24 @@
         $dayname = $data["weekday"];
         $monthname = $data["month"];
         echo "<br>";
+        // mktime(hour, minute, second, month, day, year)
+        $new_date = mktime(11, 14, 54, 8, 12, 2014);
+        // strtotime() konwersja czasu na czas unixowy
+        $unix_date=strtotime(date(""));
+        /*  Y - rok
+            m - miesiac
+            d - dzien
+            H - godzina (format 0 - 24)
+            h - (format 1 - 12)
+            i - minuty
+            s - sekundy
+            a  - am/pm
+        */
+
+        // czas do dnia
+        $d1 = strtotime("July 04");
+        $d2 = ceil(($d1-time())/60/60/24);
+        echo "There are " . $d2 ." days until 4th of July.";
 
 
         /* self <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>"> */
@@ -253,26 +272,65 @@
 
 
         // regex / [...] /
-        // /^ [...] $/ 
         // regex = "/string/" identycznie
         // regex = "/string/i" duze i male litery
         // regex = "/string/m" wyszukiwanie wielowierszowe
         // regex = "/string/u" identyczny kod UTF-8
         $string1 = "qwertyuiopasdfghjklzxcvbnm";
-        $regex1 = "/qwerty/";
+        $regex1 = "/qwerty/i";
 
         echo preg_match($regex1, $string1). "<br>";
 
         $string2 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue nibh ut luctus ultricies. Proin pharetra nunc at eros commodo sollicitudin vel non nisl. In orci ex, interdum quis augue id, commodo vehicula nisi. Curabitur volutpat leo id massa tempor sagittis. In rutrum nulla velit, nec ullamcorper diam condimentum non. Mauris non mauris et purus auctor gravida. Suspendisse ac nisi ut nisl luctus auctor. Nunc suscipit blandit congue. Ut nec diam felis. Nunc molestie accumsan sollicitudin. Ut et orci facilisis, scelerisque leo ut, pellentesque orci. Maecenas bibendum, velit vel bibendum porttitor, urna quam porta justo, ut porta tortor ligula id nibh.";
-        $regex2 = "/i/";
+        $regex2 = "/i/i";
 
         echo preg_match_all($regex2, $string2). "<br>";
 
         $string3 = "qwertyxyzqwerty";
-        $regex3 = "/xyz/";
+        $regex3 = "/xyz/i";
         $newregex3 = "_";
 
         echo preg_replace($regex3, $newregex3, $string3). "<br>";
+
+        $string4 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue nibh ut luctus ultricies. Proin pharetra nunc at eros commodo sollicitudin vel non nisl. In orci ex, interdum quis augue id, commodo vehicula nisi. Curabitur volutpat leo id massa tempor sagittis. In rutrum nulla velit, nec ullamcorper diam condimentum non. Mauris non mauris et purus auctor gravida. Suspendisse ac nisi ut nisl luctus auctor. Nunc suscipit blandit congue. Ut nec diam felis. Nunc molestie accumsan sollicitudin. Ut et orci facilisis, scelerisque leo ut, pellentesque orci. Maecenas bibendum, velit vel bibendum porttitor, urna quam porta justo, ut porta tortor ligula id nibh.";
+        $regex4 = "/[abc]/"; //a || b || c
+
+        echo preg_match_all($regex4, $string4). "<br>";
+
+        $string5 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue nibh ut luctus ultricies. Proin pharetra nunc at eros commodo sollicitudin vel non nisl. In orci ex, interdum quis augue id, commodo vehicula nisi. Curabitur volutpat leo id massa tempor sagittis. In rutrum nulla velit, nec ullamcorper diam condimentum non. Mauris non mauris et purus auctor gravida. Suspendisse ac nisi ut nisl luctus auctor. Nunc suscipit blandit congue. Ut nec diam felis. Nunc molestie accumsan sollicitudin. Ut et orci facilisis, scelerisque leo ut, pellentesque orci. Maecenas bibendum, velit vel bibendum porttitor, urna quam porta justo, ut porta tortor ligula id nibh.";
+        $regex5 = "/[^abc]/"; // wszystko poza a || b || c
+
+        echo preg_match_all($regex5, $string5). "<br>";
+
+        $string6 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue nibh ut luctus ultricies. Proin pharetra nunc at eros commodo sollicitudin vel non nisl. In orci ex, interdum quis augue id, commodo vehicula nisi. Curabitur volutpat leo id massa tempor sagittis. In rutrum nulla velit, nec ullamcorper diam condimentum non. Mauris non mauris et purus auctor gravida. Suspendisse ac nisi ut nisl luctus auctor. Nunc suscipit blandit congue. Ut nec diam felis. Nunc molestie accumsan sollicitudin. Ut et orci facilisis, scelerisque leo ut, pellentesque orci. Maecenas bibendum, velit vel bibendum porttitor, urna quam porta justo, ut porta tortor ligula id nibh.";
+        $regex6 = "/lo|ip|do/"; // wszystkie lo || ip || do
+
+        echo preg_match_all($regex6, $string6). "<br>";
+
+        $string7 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue nibh ut luctus ultricies. Proin pharetra nunc at eros commodo sollicitudin vel non nisl. In orci ex, interdum quis augue id, commodo vehicula nisi. Curabitur volutpat leo id massa tempor sagittis. In rutrum nulla velit, nec ullamcorper diam condimentum non. Mauris non mauris et purus auctor gravida. Suspendisse ac nisi ut nisl luctus auctor. Nunc suscipit blandit congue. Ut nec diam felis. Nunc molestie accumsan sollicitudin. Ut et orci facilisis, scelerisque leo ut, pellentesque orci. Maecenas bibendum, velit vel bibendum porttitor, urna quam porta justo, ut porta tortor ligula id nibh.";
+        $regex7 = "/./"; // wszystkie znaki
+
+        echo preg_match_all($regex7, $string7). "<br>". strlen($string7). "<br>";
+
+        $string8 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue nibh ut luctus ultricies. Proin pharetra nunc at eros commodo sollicitudin vel non nisl. In orci ex, interdum quis augue id, commodo vehicula nisi. Curabitur volutpat leo id massa tempor sagittis. In rutrum nulla velit, nec ullamcorper diam condimentum non. Mauris non mauris et purus auctor gravida. Suspendisse ac nisi ut nisl luctus auctor. Nunc suscipit blandit congue. Ut nec diam felis. Nunc molestie accumsan sollicitudin. Ut et orci facilisis, scelerisque leo ut, pellentesque orci. Maecenas bibendum, velit vel bibendum porttitor, urna quam porta justo, ut porta tortor ligula id nibh.";
+        $regex8 = "/^Lorem/"; // pierwszy znak
+
+        echo preg_match_all($regex8, $string8). "<br>";
+
+        $string9 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue nibh ut luctus ultricies. Proin pharetra nunc at eros commodo sollicitudin vel non nisl. In orci ex, interdum quis augue id, commodo vehicula nisi. Curabitur volutpat leo id massa tempor sagittis. In rutrum nulla velit, nec ullamcorper diam condimentum non. Mauris non mauris et purus auctor gravida. Suspendisse ac nisi ut nisl luctus auctor. Nunc suscipit blandit congue. Ut nec diam felis. Nunc molestie accumsan sollicitudin. Ut et orci facilisis, scelerisque leo ut, pellentesque orci. Maecenas bibendum, velit vel bibendum porttitor, urna quam porta justo, ut porta tortor ligula id nibh.";
+        $regex9 = "/nibh.$/"; // ostatni znak
+
+        echo preg_match_all($regex9, $string9). "<br>";
+
+        $string10 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue nibh ut luctus ultricies. Proin pharetra nunc at eros commodo sollicitudin vel non nisl. In orci ex, interdum quis augue id, commodo vehicula nisi. Curabitur volutpat leo id massa tempor sagittis. In rutrum nulla velit, nec ullamcorper diam condimentum non. Mauris non mauris et purus auctor gravida. Suspendisse ac nisi ut nisl luctus auctor. Nunc suscipit blandit congue. Ut nec diam felis. Nunc molestie accumsan sollicitudin. Ut et orci facilisis, scelerisque leo ut, pellentesque orci. Maecenas bibendum, velit vel bibendum porttitor, urna quam porta justo, ut porta tortor ligula id nibh.";
+        $regex10 = "/ \d /"; // cyfra
+
+        echo preg_match_all($regex10, $string10). "<br>";
+
+        $string11 = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus congue nibh ut luctus ultricies. Proin pharetra nunc at eros commodo sollicitudin vel non nisl. In orci ex, interdum quis augue id, commodo vehicula nisi. Curabitur volutpat leo id massa tempor sagittis. In rutrum nulla velit, nec ullamcorper diam condimentum non. Mauris non mauris et purus auctor gravida. Suspendisse ac nisi ut nisl luctus auctor. Nunc suscipit blandit congue. Ut nec diam felis. Nunc molestie accumsan sollicitudin. Ut et orci facilisis, scelerisque leo ut, pellentesque orci. Maecenas bibendum, velit vel bibendum porttitor, urna quam porta justo, ut porta tortor ligula id nibh.";
+        $regex11 = "/ \s /"; // spacja
+
+        echo preg_match_all($regex11, $string11). "<br>";
 
         $az = "/^[a-z]+$/";
         $string1 = "qwertyuiopasdfghjklzxcvbnm";
